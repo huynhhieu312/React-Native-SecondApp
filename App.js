738b9621cc3dbd5ee2d146297723ba6399ebae56
huynhhieu312 <huynh.hieu312@gmail.com>
2017-09-21 11:51:56 +0700

    secondabvb

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component,PureComponent } from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -16,22 +16,70 @@ const welcomeText =<Text>Phát cá tra</Text>
 class Welcome extends Component {
   render(){
       return(
-        <Text>Welcome {this.props.name} </Text> 
+        <Text style={ styles.personTexColor }  >Welcome {this.props.name} </Text> 
       )
   }
 }
 //event property
 class Person  extends Component {
+  // gọi hàm dựng contructor()
+  constructor(props){
+    super(props);
+    this.state = props;
+    //state va props ;
+    //avoid uppdating state and props directly;
+    //state có setState of mind  được thay đổi dữ liệu 
+  }
+//Tạo function
+  onTouch(){
+       this.setState({liked: this.state.liked + 1 });
+  }
+
   render(){
+     console.log('re-render');
     return (
-      <TouchableOpacity>
+        
+      <TouchableOpacity onPress={this.onTouch.bind(this)}>
           <View>
-            <Text>Name : {this.props.name }, liked: { this.props.liked }</Text>
+     
+           <Text style={[ styles.personTexColor , styles.personBgColor,{...this.props.personstyle} ]}>Name : {this.state.name }, liked: { this.state.liked }</Text>
          </View>
       </TouchableOpacity>
     )
   }
 }
+
+class Person1  extends PureComponent {
+  
+  constructor(props){
+    super(props);
+    this.state = props;
+     }
+
+  onTouch(){
+       this.setState({liked: this.state.liked + 1 });
+  }
+  shouldComponentUpdate(){
+    return true;
+  }
+  render(){
+     console.log('re-render');
+    return (
+        //thay đổi style = props;
+      <TouchableOpacity onPress={this.onTouch.bind(this)}>
+          <View>
+            
+           <Text style={[styles.personTexColor,{color:this.props.textcolor} ]} >Name : {this.state.name }, liked: { this.state.liked }</Text>
+        
+         </View>
+      </TouchableOpacity>
+    )
+  }
+}
+//pure compornent 
+// pure comporent 1 dạng sử lý thông minh khi trước khi nội dung được render lại thì nó sẽ kiểm trả 
+// nếu ko thay doi no' sẽ giữ nguyên và không làm gì hết
+
 export default class SecondApp extends Component {
   render() {
     return (
@@ -41,14 +89,25 @@ export default class SecondApp extends Component {
         {welcomeText}
         <Welcome name="ho chi minh City"  />
          <Person name="Khang" liked={ 100 }  />
-         <Person name="Hieu" liked={ 50 }  />
-         <Person name="Hung" liked={ 30 }  />
+         <Person name="Hieu" liked={ 50 } personstyle ={{color:'#7e1f8f'}}  />
+         <Person name="Hung" liked={ 30 } personstyle ={{color:'#7e1f8f'}}  />
+         
+          <Person1 name="per1" liked={ 30 }  textcolor ="red" />
+          
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+ //tạo class
+ personTexColor:{
+   color:'#35c'
+ },
+ personBgColor:{
+   color:"#fff",
+   backgroundColor:"#090"
+ },
   container: {
     flex: 1,
     justifyContent: 'center',
